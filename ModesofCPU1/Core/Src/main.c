d24393c6dea734016c37d04c1eb0b23594ad8268
AnuragBisht <19anurag97@gmail.com>
2026-02-21 00:33:07 +0530

    Switch from thread to Handler mode. -Added NOTES.md file.

---
+++ ModesofCPU1/Core/Src/main.c
@@ -0,0 +1,136 @@
+#include "main.h"
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void PeriphCommonClock_Config(void);
+void generate_interrupt(void);
+void RTC_WKUP_IRQHandler(void);
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Configure the peripherals common clocks */
+  PeriphCommonClock_Config();
+
+  printf("In thread mode : before interrupt\n");
+
+  generate_interrupt();
+
+  printf("In thread mode : after interrupt\n");
+
+  /* Infinite loop */
+  while (1)
+  {
+
+  }
+}
+
+void generate_interrupt()
+{
+	 volatile uint32_t * const pSTIR  = (uint32_t*)0xE000EF00;
+	 volatile uint32_t * const pISER0 = (uint32_t*)0xE000E100;
+
+	//enable IRQ3 interrupt
+	*pISER0 |= ( 1 << 3);
+
+	//generate an interrupt from software for IRQ3
+	*pSTIR = (3 & 0x1FF);
+}
+
+/* This function(ISR) executes in HANDLER MODE of the processor */
+void RTC_WKUP_IRQHandler(void)
+{
+	printf("In handler mode : ISR\n");
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_10;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK4|RCC_CLOCKTYPE_HCLK2
+                              |RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK2Divider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK4Divider = RCC_SYSCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Peripherals Common Clock Configuration
+  * @retval None
+  */
+void PeriphCommonClock_Config(void)
+{
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Initializes the peripherals clock
+  */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SMPS;
+  PeriphClkInitStruct.SmpsClockSelection = RCC_SMPSCLKSOURCE_HSI;
+  PeriphClkInitStruct.SmpsDivSelection = RCC_SMPSCLKDIV_RANGE0;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
---
+++ ModesofCPU1/NOTES.md
@@ -0,0 +1,36 @@
+**PSR Register**
+*Change in CPU register indicates which exception occured.*
+
+*Thread mode*
+Name : xpsr
+	Hex:0x61000000
+	Decimal:1627389952
+	Octal:014100000000
+	Binary:1100001000000000000000000000000
+	Default:1627389952
+
+*Handler mode*(Sys exception or IRQ)
+Name : xpsr
+	Hex:0x1000013
+	Decimal:16777235
+	Octal:0100000023
+	Binary:1000000000000000000010011
+	Default:16777235
+
+**Function calls**
+
+08000192:   bl      0x80001a8 <generate_interrupt>
+
+080001d2:   bx      lr
+
+- **LR**: Holds the return address when a function call (BL or BLX) is made.
+
+- When branching (BL), **PC** is updated with the target address, while LR stores the return address.
+
+On the ARM Cortex‑M4, when the processor enters Handler mode (e.g., during an interrupt or exception),
+the Link Register (LR) doesn’t hold a normal return address. 
+Instead, it is loaded with a special “EXC_RETURN” value — which looks like a “negative” value because its high bits are set.
+*(e.g., 0xFFFFFFF9, 0xFFFFFFFD, 0xFFFFFFF1)*.
+
+
+
