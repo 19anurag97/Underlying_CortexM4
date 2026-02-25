02eaec3f6dabb0c17440252cd5dc7e907b19f2e5
AnuragBisht <19anurag97@gmail.com>
2026-02-26 02:02:44 +0530

    IRQ handler and priority set activity added.

--- 02_NVIC_Program/Core/Src/main.c
+++ 02_NVIC_Program/Core/Src/main.c
@@ -15,54 +15,40 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#define USART1_IRQNO 36
-/* USER CODE END Includes */
+#define USART1_IRQNO    36
+#define TIM2_IRQn       28
+#define I2C1_ER_IRQn    31
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+volatile int x=0;
+/* NVIC register addresses. Refer to the processor generic guide */
+volatile uint32_t *pNVIC_IPRBase =  (volatile uint32_t*)0xE000E400;
+volatile uint32_t *pNVIC_ISERBase = (volatile uint32_t*)0xE000E100;
+volatile uint32_t *pNVIC_ISPRBase = (volatile uint32_t*)0xE000E200;
 
-/* USER CODE END PTD */
+void configure_priority_for_irqs(uint8_t irq_no, uint8_t priority_value)
+{
+	//1. find out iprx
+	uint8_t iprx = irq_no / 4;
+	volatile uint32_t *ipr =  pNVIC_IPRBase+iprx;
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+	//2. position in iprx
+	uint8_t pos = (irq_no % 4) * 8;
 
-/* USER CODE END PD */
+	//3. configure the priority
+	*ipr &= ~(0xFF << pos);//clear
+	*ipr |=  (priority_value << pos);
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
+}
 
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
+/*
+    Set a simple USART1 IRQ handler
+*/
+void Activity1(void)
 {
   //1. Manually pend the pending bit for the USART1 IRQ number in NVIC
 	uint32_t *pISPR1 = (uint32_t*)0XE000E204; //0XE000E200+4
@@ -74,6 +60,60 @@ int main(void)
 	//2. Enable the USART1 IRQ number in NVIC
 	uint32_t *pISER1 = (uint32_t*)0xE000E104; //0xE000E100+4
 	*pISER1 |= ( 1 << (USART1_IRQNO % 32));
+}
+
+/*When two interrupts with the same priority are pending,
+    the one with the lower IRQ number will be served first. In this case,
+    TIM2_IRQn has a lower IRQ number than I2C1_ER_IRQn,
+    so TIM2_IRQHandler will be executed before I2C1_ER_IRQHandler when both interrupts are pending.
+ */
+void Activity2(void)
+{
+  //1. Lets configure the priority for the peripherals
+	 configure_priority_for_irqs(TIM2_IRQn,0x80);
+	 configure_priority_for_irqs(I2C1_ER_IRQn,0x80); 
+
+	//2. Set the interrupt pending bit in the NVIC PR
+	 *pNVIC_ISPRBase |= ( 1 << TIM2_IRQn);
+
+	//3. Enable the IRQs in NVIC ISER
+	 *pNVIC_ISERBase |= ( 1 << I2C1_ER_IRQn);
+	 *pNVIC_ISERBase |= ( 1 << TIM2_IRQn);
+}
+
+/*When two interrupts with different priorities are pending,
+    the one with the higher priority (lower numerical value) will be served first. In this case,
+    I2C1_ER_IRQn has a higher priority than TIM2_IRQn,
+    so I2C1_ER_IRQHandler will be executed before TIM2_IRQHandler when both interrupts are pending.
+*/
+void Activity3(void)
+{
+  //1. Lets configure the priority for the peripherals
+	 configure_priority_for_irqs(TIM2_IRQn,0x80);
+	 configure_priority_for_irqs(I2C1_ER_IRQn,0x70); 
+
+	//2. Set the interrupt pending bit in the NVIC PR
+	 *pNVIC_ISPRBase |= ( 1 << TIM2_IRQn);
+
+	//3. Enable the IRQs in NVIC ISER
+	 *pNVIC_ISERBase |= ( 1 << I2C1_ER_IRQn);
+	 *pNVIC_ISERBase |= ( 1 << TIM2_IRQn);
+}
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  //Activity1();
+
+
+  //Activity2();
+
+
+  Activity3();
 
   while (1)
   {
@@ -83,105 +123,20 @@ int main(void)
 
 void USART1_IRQHandler(void)
 {
-	printf("in USART1 isr\n");
+	;//printf("in USART1 isr\n");
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+void TIM2_IRQHandler(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  //printf("[TIM2_IRQHandler]\n");
+  /*Here, we are pending I2C interrupt request manually */
+  *pNVIC_ISPRBase |= ( 1 << I2C1_ER_IRQn);
+  while(1);
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_10;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK4|RCC_CLOCKTYPE_HCLK2
-                              |RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLK2Divider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLK4Divider = RCC_SYSCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
 }
 
-/**
-  * @brief Peripherals Common Clock Configuration
-  * @retval None
-  */
-void PeriphCommonClock_Config(void)
+
+void I2C1_ER_IRQHandler(void)
 {
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SMPS;
-  PeriphClkInitStruct.SmpsClockSelection = RCC_SMPSCLKSOURCE_HSI;
-  PeriphClkInitStruct.SmpsDivSelection = RCC_SMPSCLKDIV_RANGE0;
-
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN Smps */
-
-  /* USER CODE END Smps */
+  x++;//printf("[I2C1_EV_IRQHandler]\n");
 }
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
